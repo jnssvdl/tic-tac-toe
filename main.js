cc175bfdbb1cbc618bf7c7c96bc9ab34a8fe4b50
Joeniesus Vidal <132261842+jnssvdl@users.noreply.github.com>
2023-10-04 23:18:36 +0800

    check if game is over

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const Gameboard = (() => {
     const addMark = (index, mark) => {
         if (gameboard[index] === '') {
             gameboard[index] = mark;
+            gameController.switchPlayer(index);
         }
     };
 
@@ -19,20 +20,16 @@ const Gameboard = (() => {
 
 const displayController = (() => {
     const cells = document.querySelectorAll('.cell');
-
-    // const render = () => {
-    //     cells.forEach((cell, index) => {
-    //         cell.textContent = Gameboard.getGameboard()[index];
-    //     });
-    // };
     
     const placeMark = () => {
         cells.forEach((cell, index) => {
             cell.addEventListener('click', () => {
                 Gameboard.addMark(index, gameController.getCurrentPlayer().getMark());
                 cell.textContent = Gameboard.getGameboard()[index];
-                // render();
-                gameController.switchPlayer();
+                const winner = gameController.checkWinner();
+                if (winner) {
+                    alert(winner);
+                }
             });
         });
     };
@@ -52,17 +49,57 @@ const Player = (name, mark) => {
 const gameController = (() => {
     const playerX = Player('brandon', 'x');
     const playerO = Player('daniel', 'o');
-    
+
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    let playerXMoves = [];
+    let playerOMoves = [];
+
     let currentPlayer = playerX;
 
     const getCurrentPlayer = () => currentPlayer;
 
-    displayController.placeMark();
-    
-    const switchPlayer = () => currentPlayer = currentPlayer === playerX ? playerO : playerX;
-
-    return {
-      getCurrentPlayer,
-      switchPlayer  
+    const switchPlayer = (move) => {
+        if (currentPlayer === playerX) {
+            playerXMoves.push(move);
+            currentPlayer = playerO;
+        } else {
+            currentPlayer = playerX;
+            playerOMoves.push(move);
+        }
     };
+
+    const isDraw = () => {
+        return Gameboard.getGameboard().every(cell => cell !== '');
+    };
+    
+    const checkWinner = () => {
+        let winner;
+        for (let winningCondition of winningConditions) {
+            if (winningCondition.every((i) => playerXMoves.includes(i))) {
+                return "Player X Wins";
+            }
+            if (winningCondition.every((i) => playerOMoves.includes(i))) {
+                return "Player O Wins";
+            }
+        }
+    
+        if (isDraw()) {
+            return "It's a Draw";
+        }
+ 
+    };
+
+    displayController.placeMark();
+
+    return {getCurrentPlayer, switchPlayer, checkWinner};
 })();
